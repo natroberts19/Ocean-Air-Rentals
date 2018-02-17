794e1ec8349eb3b4834972badf41238e7748c60c
Natalie Roberts <nr_roberts@yahoo.com>
2018-02-17 10:49:43 -0500

    moved folders, added servergabe.js

--- routes/api-inventory-routes.js
+++ routes/api-inventory-routes.js
@@ -1,11 +1,4 @@
-// For inventory!
+// These routes will allow the manager to update inventory.
 
-// app.get("/api//beach:name",)...
-// var name = req.params.name
-// var g = google call
-// var w = weather call
-// res.json(data)
-// This route will package Google and weather data (see picture)
-// var data = {
-    // beachname: g.beachname.rating, etc.
-    // weather: w.day1, day2, etc.
+app.get("/api/manager",)
+// 
