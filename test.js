c5dc31730f90b354c894c3fef5f9a7159a5a40b8
Gabriel Vermund <gabrielvermund@Gabriels-MacBook-Pro.local>
2018-02-20 18:05:46 -0500

    some testing stuff

--- test.js
+++ test.js
@@ -1,25 +1,23 @@
 var expect = require("chai").expect;
 var axios = require("axios");
-var main = require("./public/javascript/main.js");
-var testFunction = function (event) {
-    
 
-    var newReservation = {
-        reservationName: ("Gabe"),
-        reservationEmail: ("gvermund@aol.com"),
-        reservationPhone: ("2055662453"),
-        reservationBeach: ("Cocoa"),
-        reservationItem: ("Chairs and Umbrella"),
-        reservationDate: ("02-18-2018")
-    };
+var newReservation = {
+    reservationName: ("Gabe"),
+    reservationEmail: ("gvermund@aol.com"),
+    reservationPhone: ("2055662453"),
+    reservationBeach: ("Cocoa"),
+    reservationItem: ("Chairs and Umbrella"),
+    reservationDate: ("02-18-2018")
 
-    
+};
 
+var testFunction = async function (event) {
     // submitReservation(newReservation);
     
     // function submitReservation(newReservation) {
         axios.post("/api/reservation", newReservation, function(result) {
             console.log(result);
+            return newReservation;
         });
     
     //   }
@@ -27,6 +25,6 @@ var testFunction = function (event) {
 };
 describe("testFunction", function(){
     it("should make a new reservation based on user input", function(){
-        expect(testFunction).to.exist(newReservation);
+        expect(testFunction()).to.equal(newReservation);
     });
 });
